3d33da1e1eb0766d512ee08293c0133930659ece
RyoIwahara <ryo.iwahara1010@outlook.jp>
2022-11-23 23:24:16 +0900

    実行時のマイグレーション設定

--- cmd/mt-api-go/main.go
+++ cmd/mt-api-go/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func main() {
-	fmt.Println("Hello,World!")
+
+	// db-migrationの実行
+	// コマンドラインの場合
+	//  migrate -database 'postgres://mtadmin:mtadmin@localhost:5433/muscletracking?sslmode=disable' -path tools/db/migrations up 実行
+	//  migrate -database 'postgres://mtadmin:mtadmin@localhost:5433/muscletracking?sslmode=disable' -path tools/db/migrations down 取消
+	m, err := migrate.New(
+		"file://tools/db/migrations",
+		"postgres://mtadmin:mtadmin@localhost:5433/muscletracking?sslmode=disable")
+
+	if err != nil {
+		fmt.Println("err1")
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if err != migrate.ErrNoChange {
+			fmt.Println("err2")
+			panic(err)
+		}
+	}
+
 }
